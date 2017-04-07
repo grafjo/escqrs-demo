aff9ea0f5df75ff4003edc8c9b18e3547ed6ab34
Johannes Graf <graf@synyx.de>
2017-04-07 12:32:23 +0200

    add BacklogItemCreatedEvent

---
+++ src/main/java/org/synyx/campdemo/write/agileproject/domain/event/BacklogItemCreatedEvent.java
@@ -0,0 +1,21 @@
+package org.synyx.campdemo.write.agileproject.domain.event;
+
+
+public class BacklogItemCreatedEvent {
+
+    private final String identifier;
+    private final String name;
+
+    public BacklogItemCreatedEvent(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
