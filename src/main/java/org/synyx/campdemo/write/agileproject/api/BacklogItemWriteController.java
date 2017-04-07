4589f0f17a6283f05ad94d3fabf58f8866b843a6
Johannes Graf <graf@synyx.de>
2017-04-07 12:33:07 +0200

    implement BacklogItemWriteController.create()

--- src/main/java/org/synyx/campdemo/write/agileproject/api/BacklogItemWriteController.java
+++ src/main/java/org/synyx/campdemo/write/agileproject/api/BacklogItemWriteController.java
@@ -1,5 +1,9 @@
 package org.synyx.campdemo.write.agileproject.api;
 
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.common.IdentifierFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -9,16 +13,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.synyx.campdemo.write.agileproject.domain.command.CreateBacklogItemCommand;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 
 @RestController
 @RequestMapping("/backlogitems")
 public class BacklogItemWriteController {
 
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public BacklogItemWriteController(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> create(@RequestBody BacklogItemDto backlogItem) {
 
-        throw new UnsupportedOperationException("Here is something missing. Fix it!");
+        String identifier = IdentifierFactory.getInstance().generateIdentifier();
+        CreateBacklogItemCommand command = new CreateBacklogItemCommand(identifier, backlogItem.name);
+
+        commandGateway.sendAndWait(command);
+
+        URI uri = fromCurrentRequest().path("/{id}").buildAndExpand(identifier).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
     }
 
 
