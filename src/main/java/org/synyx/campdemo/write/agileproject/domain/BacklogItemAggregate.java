2136a631748d953c251f161998929d7e589ffe73
Johannes Graf <graf@synyx.de>
2017-04-07 14:02:32 +0200

    fire BacklogItemCommitEvent

--- src/main/java/org/synyx/campdemo/write/agileproject/domain/BacklogItemAggregate.java
+++ src/main/java/org/synyx/campdemo/write/agileproject/domain/BacklogItemAggregate.java
@@ -5,14 +5,17 @@ import org.axonframework.commandhandling.model.AggregateIdentifier;
 import org.axonframework.commandhandling.model.AggregateLifecycle;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.synyx.campdemo.write.agileproject.domain.command.CommitBacklogItemCommand;
 import org.synyx.campdemo.write.agileproject.domain.command.CreateBacklogItemCommand;
 import org.synyx.campdemo.write.agileproject.domain.event.BacklogItemCreatedEvent;
+import org.synyx.campdemo.write.agileproject.domain.event.BacklogItemCommitEvent;
 
 @Aggregate
 public class BacklogItemAggregate {
 
     @AggregateIdentifier
     private String identifier;
+    private String sprintidentifier;
 
     public BacklogItemAggregate() {
         // no-arg constructor required by Axon
@@ -24,8 +27,19 @@ public class BacklogItemAggregate {
         AggregateLifecycle.apply(new BacklogItemCreatedEvent(command.getIdentifier(), command.getName()));
     }
 
+    @CommandHandler
+    public void commit(CommitBacklogItemCommand command) {
+
+        AggregateLifecycle.apply(new BacklogItemCommitEvent(command.getSprintidentifier(), command.getBacklogItemidentifier()));
+    }
+
     @EventSourcingHandler
     private void handle(BacklogItemCreatedEvent event) {
         this.identifier = event.getIdentifier();
+    }
+
+    @EventSourcingHandler
+    private void handle(BacklogItemCommitEvent event) {
+        this.sprintidentifier = event.getSprintidentifier();
     }
 }
